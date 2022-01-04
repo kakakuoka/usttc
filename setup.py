c9ae71a76cee898f1eebcaa3116eb92c02ffed13
kakakuoka <kuo.zh92@gmail.com>
2022-01-04 15:05:44 -0600

    remove websocket package version requirement

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="usttc",
-    version="0.0.1",
+    version="0.0.2",
     author="Kuo Zhang",
     author_email="kuo.zh92@gmail.com",
     description="Unified Speech-to-text Client",
-    download_url='https://github.com/kakakuoka/usttc/archive/refs/tags/v0.0.1.tar.gz',
+    download_url='https://github.com/kakakuoka/usttc/archive/refs/tags/v0.0.2.tar.gz',
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=requirements,
